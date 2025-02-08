2bf816aef19399f33bdef4c8f96803d0864f520c
Carlos Dourado <carlosismaelad@outlook.com>
2025-02-08 17:14:35 -0300

    Adds imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { UtilsService } from './utils/utils.service';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, UtilsService],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, UtilsService],
 })
 export class AppModule {}
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
+import { UtilsService } from 'src/utils/utils.service';
+import { UserRepository } from './repository/user.repository';
+import { GenericSimpleResult } from 'src/utils/generic-simple-result.service';
 
 @Module({
+  imports: [UtilsService, GenericSimpleResult],
   controllers: [UserController],
   providers: [UserService],
+  exports: [UserService, UserRepository],
 })
 export class UserModule {}
