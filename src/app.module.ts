bdcc4e852a8867ac21e6de2ec4e42c33415ee50f
Carlos Dourado <carlosismaelad@outlook.com>
2025-02-09 20:04:47 -0300

    adds environment variables and imports UserModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user/entities/user.entity';
+import 'dotenv/config';
 import { UtilsService } from './utils/utils.service';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [UserModule, UtilsService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.HOST,
+      port: process.env.PG_PORT ? parseInt(process.env.PG_PORT, 10) : undefined,
+      username: process.env.PG_USER,
+      password: process.env.PG_PASS,
+      database: process.env.PG_DB,
+      entities: [User],
+      synchronize: false,
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService, UtilsService],
 })
